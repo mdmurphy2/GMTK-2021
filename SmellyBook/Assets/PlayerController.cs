b04abec712158b4faae49e2b2f40142903dbaf0e
Matthew Murphy <mdmurphy2@wisc.edu>
2021-06-11 19:13:42 -0500

    Added Double Jump :smile:

--- SmellyBook/Assets/PlayerController.cs
+++ SmellyBook/Assets/PlayerController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Rigidbody2D))] 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] Rigidbody2D rigidbody2D;
+    [SerializeField] CapsuleCollider2D capsuleCollider2D;
+    public LayerMask groundLayer;
     public float maxSpeed = 10f;
     public float speed = 5f;
     public float jumpForce = 30f;
-    
+
     private bool canDoubleJump = true;
 
     private bool grounded = true;
@@ -24,43 +26,55 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void Update() {
+    private void Update()
+    {
+        CheckJump();
 
-        if(Input.GetButtonDown("Jump")) {
-            if(grounded) {
-                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
-                rigidbody2D.AddForce(new Vector2(0, jumpForce));
-                canDoubleJump = true;
-            }else {
-                if (canDoubleJump) {
-                    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
-                    rigidbody2D.AddForce(new Vector2(0, jumpForce));
-                    canDoubleJump = false;
-                }
-            }
-        }
-
-
-        if(Mathf.Approximately(rigidbody2D.velocity.y,0)) {
-            canDoubleJump = true;
-        }
-
-        if(Input.GetButtonDown("Jump") && (canDoubleJump || Mathf.Approximately(rigidbody2D.velocity.y,0))){
-            
-            rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-
-        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
+        MoveHorizontal();
+
+
+    }
+
+    private void MoveHorizontal()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
-
-
         rigidbody2D.velocity = new Vector2(horizontal * speed, rigidbody2D.velocity.y);
-        
-        
+    }
+
+    private void CheckJump() {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded())
+            {
+                Debug.Log("JUMP1");
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
+                rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                canDoubleJump = true;
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    Debug.Log("JUMP2");
+                    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
+                    rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                    canDoubleJump = false;
+                }
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float extraHeight = .01f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(capsuleCollider2D.bounds.center, Vector2.down, capsuleCollider2D.bounds.extents.y + extraHeight, groundLayer);
+
+        return raycastHit.collider != null;
     }
 }
